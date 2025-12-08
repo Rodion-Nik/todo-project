f299e65fa1ec0984536b24fcb801792ce848ecc8
Rodion <rodion.nik19@gmail.com>
2025-12-08 19:51:57 +0100

    improve: rework core components

--- to-do.js
+++ to-do.js
@@ -2,13 +2,14 @@ const AddDiv = document.querySelector('#AddDiv')
 const TodoInput = document.querySelector('#todoInput')
 const todoListHolder = document.querySelector('#todoListHolder')
 
+let formText = ""
+
 //тут значения из ModalWin
 let ModalInputNameValue;
 let ModalInputDateValue; 
 let ModalInputMainInfoValue; 
-
 let modalCheck = true
-
+// устаревшая функция, мб использую потом
 function ModalWin() {
     return new Promise(function(resolve) {
         // Просто структура модального окна
@@ -66,21 +67,105 @@ function ModalWin() {
 }
 
 
+const createtodo = document.createElement('div');
+createtodo.className="createtodo";
+createtodo.textContent = "+ добавить колонку";
+todoListHolder.append (createtodo);
+let createTodoColumnInputValue = ""
+function createTodoColumn(){
+    createtodo.classList.add('editing');
+    createtodo.innerHTML = `
+    <div>
+        <textarea placeholder="Введите название" class="CreateColumInput"></textarea>
+        <div class="CreateColumAcept">Принять</div>
+    </div>
+    `
+    const createTodoColumnInput =  createtodo.querySelector(".CreateColumInput")
+    
 
-AddDiv.addEventListener('click', function()  {
+    const CreateColumAcept = createtodo.querySelector('.CreateColumAcept')
+    CreateColumAcept.addEventListener ('click', function (e) {
+        e.stopPropagation(); 
+        createTodoColumnInputValue = createTodoColumnInput.value
+        createtodo.classList.remove("editing");
+        createtodo.innerHTML = ``
+        createtodo.className = "createtodo";
+        createtodo.textContent = "+ добавить колонку";
+
+    })
+
+}
+
+createtodo.addEventListener('click', function(ee)  {
+     ee.stopPropagation(); 
+
+    if (createtodo.classList.contains('editing')) return;
+    createTodoColumn();
     // основной todo div 
     const todo = document.createElement('div');
-    todo.className = "todo todoItem"
+    todo.className = "todo"
 
+    //тоже самое что и в карточке только для колонки
+    
+    
     // text из input
-    let text = TodoInput.value
+    
+    
+    
+    //карточка "создать карточку"
+    const AddTodoCard = document.createElement('div')
+    AddTodoCard.textContent = "+ создать карточку"
+    AddTodoCard.className = "AddTodoCard"
+
+    //имя основного блока
     const todoname = document.createElement('div');
-    todoname.textContent = text
+    todoname.className ="todoname"
+    todoname.textContent = createTodoColumnInputValue
+    todoname.append(AddTodoCard)
+
+
+    const form = document.createElement('div')
+    form.innerHTML = `
+        <div>
+            <textarea placeholder="Введите название" class="formInput"></textarea>
+            <div class="formAcept">Принять</div>
+        </div>
+        `
+
+    
+    const formInputValue = form.querySelector(".formInput")
+    formInputValue.addEventListener("input", () => {
+        formInputValue.style.height = "auto";           // сбрасываем высоту
+    formInputValue.style.height = formInputValue.scrollHeight + "px"; // устанавливаем нужную
+    });
+    const formAcept = form.querySelector(".formAcept")
+    AddTodoCard.addEventListener('click', function() {        
+        AddTodoCard.textContent = ""
+        AddTodoCard.className=""
+        form.addEventListener('click', function(e) {
+            e.stopPropagation(); 
+        });
+        AddTodoCard.append(form)
+
+
+
+        formAcept.addEventListener ('click', async function() {
+            
+            
+            
+            AddTodoCard.textContent = "+ создать карточку"
+            AddTodoCard.className = "AddTodoCard"
+            
+
+        })
+
+
+    })
 
     //троеточие в котором можно будет добавить или удалить 
     let todoSettingCheck = false
     const todoSetting = document.createElement('div')
-    
+    todoSetting.textContent = "..."
     todoSetting.className = "todoSetting"
     todoSetting.addEventListener('click', function() {
         if (todoSettingCheck == false) {
@@ -88,70 +173,67 @@ AddDiv.addEventListener('click', function()  {
         settingList.className = "SettingList"
         settingList.innerHTML = `
         <div class="SettingHolder">
-            <div class="todoCreate">Add</div>
             <div class="todoDelete">Del</div>
-        </div>
-        `
+        </div>`
         todoSetting.append(settingList)
         todoSettingCheck = true
-
-        
-        //берет значения из модалки и создает карточки
-        const todoCreate = todoSetting.querySelector(".todoCreate")
-        todoCreate.addEventListener ('click',async function() {
-            await ModalWin();
-            const todoAddDiv = document.createElement('div');
-            todoAddDiv.innerHTML = `
-            <div class = 'todoAddedDiv'>
-                <p class="TackName"></p>
-                <p class="TackDate"></p>
-                <p class="TackText"></p>
-                <div class="CloseAddedDiv">X</div>
-            </div>
-            `;
-            todoname.appendChild(todoAddDiv)
-            const TackName = todoAddDiv.querySelector('.TackName')
-            const TackDate = todoAddDiv.querySelector('.TackDate')
-            const TackText = todoAddDiv.querySelector('.TackText')
-
-            TackName.textContent = ModalInputNameValue;
-            TackDate.textContent = ModalInputDateValue;
-            TackText.textContent = ModalInputMainInfoValue;
-            if (modalCheck == false) {
-                todoAddDiv.remove()
-            }
-            
-            const CloseAddedDiv = todoAddDiv.querySelector(".CloseAddedDiv")
-            CloseAddedDiv.addEventListener ('click', function() {
-                todoAddDiv.remove()
-            })
-            
-        });
-
-
-
         //логика кнопки удаления
-        const todoDelete = todoSetting.querySelector(".todoDelete") 
-        todoDelete.addEventListener('click', function() {
+            const todoDelete = settingList.querySelector(".todoDelete") 
+            todoDelete.addEventListener('click', function() {
             todo.remove();
         });
-
-        //закрытие 
+                //закрытие 
         }  else {
             const SettingList = todoSetting.querySelector('.SettingList')
             SettingList.remove()
             todoSettingCheck = false
         }});
-    
+
+        
+
+
+        //берет значения из модалки и создает карточки
+
+        formAcept.addEventListener ('click', function() {
+            const todoAddDiv = document.createElement('div');
+            console.log('form acept');
+            formText = formInputValue.value
+            todoAddDiv.innerHTML = `
+            <div class = 'todoAddedDiv'>
+                <div class="CloseAddedDiv">X</div>
+                <div class="AddedDivText"></div>
+                
+            </div>`;
+            const AddedDivText = todoAddDiv.querySelector(".AddedDivText")
+            AddedDivText.textContent = formText
+
+            
+            const CloseAddedDiv = todoAddDiv.querySelector(".CloseAddedDiv")
+            CloseAddedDiv.addEventListener ('click', function() {
+                todoAddDiv.remove()
+            }) 
+
+
+            todoname.appendChild(todoAddDiv)
+            todoname.append(AddTodoCard)
+
+            
+
+            });
+
+
+
+
 
 
     //сборка
     todo.append(todoname, todoSetting);
     todoListHolder.append(todo);
-
     //обнуление инпута
     TodoInput.value = '';
+    
 
+    todoListHolder.append (createtodo);
 });
 
 
