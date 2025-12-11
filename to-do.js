734d0e5a30ea3bfa7745b4e7365e67cb37581ee9
Rodion <rodion.nik19@gmail.com>
2025-12-11 18:23:33 +0100

    add a modal window for changing the name and writing descriptions for cards

--- to-do.js
+++ to-do.js
@@ -4,9 +4,10 @@ const todoListHolder = document.querySelector('#todoListHolder')
 
 let formText = ""
 
+
+
 //тут значения из ModalWin
 let ModalInputNameValue;
-let ModalInputDateValue; 
 let ModalInputMainInfoValue; 
 let modalCheck = true
 // устаревшая функция, мб использую потом
@@ -18,61 +19,93 @@ function ModalWin() {
     modal.innerHTML = `
     <div class="ModalWinMain">
         <div class="ModalInputHolder">
-            <p class="ModalText">Введите название (опцеонально)</p> 
-            <input id="ModalInputName">
-            <p class="ModalText">Введите дату</p> 
-            <input id="ModalInputDate">
-            <p class="ModalText">Описание</p> 
-            <input id="ModalInputMainInfo">
+            <header class="ModalWinHeader">
+                <div class="ModalWinClose">
+                    <span data-testid="CloseIcon" data-vc="icon-CloseIcon" aria-hidden="true" class="_1e0c1o8l _1o9zidpf _vyfuvuon _vwz4kb7n _1szv15vq _1tly15vq _rzyw1osq _17jb1osq _1ksvoz0e _3se1x1jp _re2rglyw _1veoyfq0 _1kg81r31 _jcxd1r8n _gq0g1onz _1trkwc43" style="--icon-primary-color: var(--ds-text-accent-gray-bolder, #172B4D); --icon-secondary-color: inherit;"><svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12Z" fill="currentColor"></path></svg></span>
+                </div>
+            </header>
+            <section>
+                <textarea id="ModalInputName"></textarea>
+            </section>
+            <div>
+                <p class="ModalText">Описание</p> 
+                <textarea id="ModalInputMainInfo"></textarea>
+            </div>
         </div>
         <div class="ModalActionsHolder">
             <div id="ModalOk">Ok</div>
-            <div id="ModalClose">Close</div>
         </div>
     </div>`;
     document.body.append(modal);
     // объявление всех элементов
     const ModalInputName = document.querySelector('#ModalInputName')
-    const ModalInputDate = document.querySelector('#ModalInputDate')
     const ModalInputMainInfo = document.querySelector('#ModalInputMainInfo')
     const ModalOk = document.querySelector('#ModalOk')
-    const ModalClose = document.querySelector('#ModalClose')
+
+    ModalInputMainInfo.addEventListener("input", () => {
+        const maxHeight = 200;
+
+        ModalInputMainInfo.style.height = "auto";
+
+        if (ModalInputMainInfo.scrollHeight > maxHeight) {
+            ModalInputMainInfo.style.height = maxHeight + "px";
+            ModalInputMainInfo.style.overflowY = "auto"; // включаем скролл
+        } else {
+            ModalInputMainInfo.style.height = ModalInputMainInfo.scrollHeight + "px";
+            ModalInputMainInfo.style.overflowY = "hidden"; // отключаем скролл
+        }
+    });
+    
+    ModalInputName.addEventListener("input", () => {
+        const maxHeight = 300;
+
+        ModalInputName.style.height = "auto";
+
+        if (ModalInputName.scrollHeight > maxHeight) {
+            ModalInputName.style.height = maxHeight + "px";
+            ModalInputName.style.overflowY = "auto"; // включаем скролл
+        } else {
+            ModalInputName.style.height = ModalInputName.scrollHeight + "px";
+            ModalInputName.style.overflowY = "hidden"; // отключаем скролл
+        }
+    });
+
+    ModalInputName.value = ModalInputNameValue || "";
+    ModalInputMainInfo.value = ModalInputMainInfoValue || "";
 
     //логика кнопки ок а точнее задание значений из input и удаления modal
     ModalOk.addEventListener('click', function() {
-        ModalInputNameValue = ModalInputName.value
-        ModalInputDateValue = ModalInputDate.value
         ModalInputMainInfoValue = ModalInputMainInfo.value
-        if (ModalInputNameValue.trim().length === 0) {
-        ModalInputNameValue = " "
-    }  
-        if (ModalInputDateValue.trim().length === 0 || 
-        ModalInputMainInfoValue.trim().length === 0){
-        alert("введите дату и описание")
+        ModalInputNameValue = ModalInputName.value
+
+        if ( ModalInputNameValue.trim().length === 0){
+                alert("название")
             } else {
                 modal.remove();
                 resolve();
             }
     })
     //кнопка закрытия modal окна
-    ModalClose.addEventListener('click', function() {
+    const ModalWinClose = modal.querySelector('.ModalWinClose') 
+    ModalWinClose.addEventListener('click', function() {
         modal.remove()
         modalCheck = false
-        ModalInputNameValue = ""
-        ModalInputDateValue = ""
-        ModalInputMainInfoValue = ""
         resolve();
     })
     })
 }
 
 
+
 const createtodo = document.createElement('div');
 createtodo.className="createtodo";
+
 createtodo.textContent = "+ добавить колонку";
 todoListHolder.append (createtodo);
 let createTodoColumnInputValue = ""
+// это функция заменяет "добавить колонку" на 2 дива куда нужно вписать название и после оно примется
 function createTodoColumn(){
+    return new Promise((resolve) => {
     createtodo.classList.add('editing');
     createtodo.innerHTML = `
     <div>
@@ -81,35 +114,38 @@ function createTodoColumn(){
     </div>
     `
     const createTodoColumnInput =  createtodo.querySelector(".CreateColumInput")
-    
-
+    createTodoColumnInput.addEventListener("input", () => {
+    createTodoColumnInput.style.height = "auto";           // сбрасываем высоту
+    createTodoColumnInput.style.height = createTodoColumnInput.scrollHeight + "px"; // устанавливаем нужную
+    })
     const CreateColumAcept = createtodo.querySelector('.CreateColumAcept')
     CreateColumAcept.addEventListener ('click', function (e) {
         e.stopPropagation(); 
+
         createTodoColumnInputValue = createTodoColumnInput.value
+        
+        if (createTodoColumnInputValue.trim().length >= 1 ) {
         createtodo.classList.remove("editing");
         createtodo.innerHTML = ``
         createtodo.className = "createtodo";
         createtodo.textContent = "+ добавить колонку";
-
-    })
+        resolve(createTodoColumnInputValue);}
+        
+    })}) 
 
 }
 
-createtodo.addEventListener('click', function(ee)  {
-     ee.stopPropagation(); 
+createtodo.addEventListener('click', async function()  {
+; 
 
-    if (createtodo.classList.contains('editing')) return;
-    createTodoColumn();
+    if (createtodo.classList.contains('editing')) return; 
+    await createTodoColumn();
+
     // основной todo div 
     const todo = document.createElement('div');
     todo.className = "todo"
 
     //тоже самое что и в карточке только для колонки
-    
-    
-    // text из input
-    
     
     
     //карточка "создать карточку"
@@ -123,7 +159,6 @@ createtodo.addEventListener('click', function(ee)  {
     todoname.textContent = createTodoColumnInputValue
     todoname.append(AddTodoCard)
 
-
     const form = document.createElement('div')
     form.innerHTML = `
         <div>
@@ -135,7 +170,7 @@ createtodo.addEventListener('click', function(ee)  {
     
     const formInputValue = form.querySelector(".formInput")
     formInputValue.addEventListener("input", () => {
-        formInputValue.style.height = "auto";           // сбрасываем высоту
+    formInputValue.style.height = "auto";           // сбрасываем высоту
     formInputValue.style.height = formInputValue.scrollHeight + "px"; // устанавливаем нужную
     });
     const formAcept = form.querySelector(".formAcept")
@@ -147,29 +182,59 @@ createtodo.addEventListener('click', function(ee)  {
         });
         AddTodoCard.append(form)
 
-
-
-        formAcept.addEventListener ('click', async function() {
-            
-            
-            
+        formAcept.addEventListener ('click',  function() {
             AddTodoCard.textContent = "+ создать карточку"
             AddTodoCard.className = "AddTodoCard"
-            
-
         })
-
-
     })
 
+        //берет значения  и создает карточки
+
+        formAcept.addEventListener ('click', function() {
+            const todoAddDiv = document.createElement('div');
+            formText = formInputValue.value
+            todoAddDiv.innerHTML = `
+            <div class = 'todoAddedDiv'>
+                <div class="CloseAddedDiv">X</div>
+                <div class="AddedDivText"></div>
+                
+            </div>`;
+            const AddedDivText = todoAddDiv.querySelector(".AddedDivText")
+            AddedDivText.textContent = formText
+
+            //описание
+            AddedDivText.dataset.description = "";
+            
+
+            
+            const CloseAddedDiv = todoAddDiv.querySelector(".CloseAddedDiv")
+            CloseAddedDiv.addEventListener ('click', function() {
+                todoAddDiv.remove()
+            }) 
+
+
+            todoname.appendChild(todoAddDiv)
+            todoname.append(AddTodoCard)
+            AddedDivText.addEventListener ('click', async function() {
+                ModalInputNameValue = AddedDivText.textContent
+                ModalInputMainInfoValue = AddedDivText.dataset.description || ""
+                await ModalWin()
+                AddedDivText.textContent = ModalInputNameValue
+                AddedDivText.dataset.description = ModalInputMainInfoValue
+            })
+            });
+
+        
     //троеточие в котором можно будет добавить или удалить 
     let todoSettingCheck = false
     const todoSetting = document.createElement('div')
     todoSetting.textContent = "..."
     todoSetting.className = "todoSetting"
-    todoSetting.addEventListener('click', function() {
-        if (todoSettingCheck == false) {
+
+    todoSetting.addEventListener('click', function(e) {
         const settingList = document.createElement('div')
+
+        if (todoSettingCheck == false) {
         settingList.className = "SettingList"
         settingList.innerHTML = `
         <div class="SettingHolder">
@@ -189,38 +254,6 @@ createtodo.addEventListener('click', function(ee)  {
             todoSettingCheck = false
         }});
 
-        
-
-
-        //берет значения из модалки и создает карточки
-
-        formAcept.addEventListener ('click', function() {
-            const todoAddDiv = document.createElement('div');
-            console.log('form acept');
-            formText = formInputValue.value
-            todoAddDiv.innerHTML = `
-            <div class = 'todoAddedDiv'>
-                <div class="CloseAddedDiv">X</div>
-                <div class="AddedDivText"></div>
-                
-            </div>`;
-            const AddedDivText = todoAddDiv.querySelector(".AddedDivText")
-            AddedDivText.textContent = formText
-
-            
-            const CloseAddedDiv = todoAddDiv.querySelector(".CloseAddedDiv")
-            CloseAddedDiv.addEventListener ('click', function() {
-                todoAddDiv.remove()
-            }) 
-
-
-            todoname.appendChild(todoAddDiv)
-            todoname.append(AddTodoCard)
-
-            
-
-            });
-
 
 
 
@@ -230,7 +263,6 @@ createtodo.addEventListener('click', function(ee)  {
     todo.append(todoname, todoSetting);
     todoListHolder.append(todo);
     //обнуление инпута
-    TodoInput.value = '';
     
 
     todoListHolder.append (createtodo);
